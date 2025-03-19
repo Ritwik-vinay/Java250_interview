d0452400b4f9396e669b3adfd7922d3d20705ea0
ritwik vinay <ritwikvinay38@gmail.com>
2025-03-19 09:07:47 +0530

    Interview Questions Commit :Task

---
+++ src/Arrays/sample.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class sample {
+    public static void main(String[] args) {
+        int a =5;
+
+        int b= ++a;
+        System.out.println(a);
+
+        System.out.println(b);
+
+    }
+}
