e044af50932891b7980e00363b261bab122279bf
ritwik vinay <ritwikvinay38@gmail.com>
2025-05-09 08:53:43 +0530

    Interview Questions Practice :Commit

---
+++ src/PalindromeString_Check.java
@@ -0,0 +1,19 @@
+public class PalindromeString_Check {
+
+    public static void main(String[] args) {
+        String s = "madam";
+        s.toLowerCase();
+        String rev = "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev = sb.append(s.charAt(i)).toString();
+        }
+        if (s.equals(rev)) {
+            System.out.println("It is palindrome: " + s + " :rev: " + rev);
+        }
+        else
+        {
+            System.out.println("It is not a Palindrome");
+        }
+    }
+}
