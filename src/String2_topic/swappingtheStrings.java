f08367d44e054f52c4d1b60bf64e208f20e5488f
ritwik vinay <ritwikvinay38@gmail.com>
2025-05-10 13:28:19 +0530

    Interview Questions Practice :Commit

---
+++ src/String2_topic/swappingtheStrings.java
@@ -0,0 +1,15 @@
+package String2_topic;
+
+public class swappingtheStrings {
+    public static void main(String[] args) {
+        String a="Hello";
+        String b ="World";
+        String c=b;
+        a=a+b;
+        System.out.println("Without reversing "+a+b);
+        b=a.substring(0,a.length()-b.length());
+        a=a.substring(b.length());
+        System.out.println("After reversing "+ a + b);
+
+    }
+}
