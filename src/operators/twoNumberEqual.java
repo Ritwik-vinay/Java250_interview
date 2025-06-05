21ee90cfabf969ec56c3fee76237bba6fa5b9e31
ritwik vinay <ritwikvinay38@gmail.com>
2025-06-05 23:27:21 +0530

    Interview Questions Practice :Commit

---
+++ src/operators/twoNumberEqual.java
@@ -0,0 +1,23 @@
+package operators;
+
+public class twoNumberEqual {
+    public static void main(String[] args) {
+        //2.Check if two numbers are equal using ==.
+        int a= 10;
+        int b=12;
+        int num= 10;
+        String s= "a";
+        String n= "b";
+        System.out.println(a==b);
+        System.out.println(a==num);
+        //3.Use != to check if values are not equal
+        System.out.println(a!=b);
+        System.out.println(a!=num);
+        //4.Check if a number is greater than another.
+        System.out.println(b>a);
+        System.out.println(num>b);
+        //5. Use logical AND (&&) to check two conditions.
+        System.out.println();
+
+    }
+}
