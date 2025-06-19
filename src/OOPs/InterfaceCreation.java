986c911559e7c9ceda3c1c2491ce085369644755
ritwik vinay <ritwikvinay38@gmail.com>
2025-06-19 22:35:56 +0530

    Interview Questions Practice :Commit

--- src/OOPs/InterfaceCreation.java
+++ src/OOPs/InterfaceCreation.java
@@ -1,4 +1,31 @@
 package OOPs;
 
 public class InterfaceCreation {
+    public static void main(String[] args) {
+        Animal a = new Dog();
+        Animal b = new Cat();
+        a.sound();
+        b.sound();
+
+    }
+    //An interface in Java is a contract: it defines what a class can do, but not how.
+    //It can have:
+    //Abstract methods (implicitly public and abstract)
+    //Default methods (from Java 8)
+    //Static methods
+    //Constants (public static final)
+    static class Cat implements Animal{
+        public void sound(){
+            System.out.println("Cat Meow");
+        }
+    }
+    static class Dog implements Animal {
+        public void sound(){
+            System.out.println("Dog Barks");
+        }
+    }
+   interface Animal{
+        void sound();
+
+   }
 }
