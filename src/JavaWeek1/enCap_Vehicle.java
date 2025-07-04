23390d5e54d31b8e47b029e65ba6567a9a0c0706
ritwik vinay <ritwikvinay38@gmail.com>
2025-07-04 08:25:19 +0530

    Interview Questions Practice :Commit

--- src/JavaWeek1/enCap_Vehicle.java
+++ src/JavaWeek1/enCap_Vehicle.java
@@ -1,32 +1,51 @@
 package JavaWeek1;
 
+interface Vehicle {
+    void brand(String brandName);
+    void makeModel(String makeModel);
+    void vehicleAge(int years);
+}
+
+class Bike implements Vehicle {
+    public void brand(String brandName) {
+        System.out.println("Bike Brand: " + brandName);
+    }
+
+    public void makeModel(String makeModel) {
+        System.out.println("Bike Model: " + makeModel);
+    }
+
+    public void vehicleAge(int years) {
+        System.out.println("Bike Age: " + years + " years");
+    }
+}
+
+class Car implements Vehicle {
+    public void brand(String brandName) {
+        System.out.println("Car Brand: " + brandName);
+    }
+
+    public void makeModel(String makeModel) {
+        System.out.println("Car Model: " + makeModel);
+    }
+
+    public void vehicleAge(int years) {
+        System.out.println("Car Age: " + years + " years");
+    }
+}
+
 public class enCap_Vehicle {
     public static void main(String[] args) {
-        Bajaj bajaj = new Bajaj();
-        bajaj.brand("Pulsar");
-        bajaj.makeModel("NS_2025");
-        bajaj.VehicleAge(5);
-    }
+        Vehicle bike = new Bike();
+        bike.brand("Pulsar");
+        bike.makeModel("NS_2025");
+        bike.vehicleAge(5);
 
+        System.out.println("----------");
+
+        Vehicle car = new Car();
+        car.brand("Toyota");
+        car.makeModel("Fortuner");
+        car.vehicleAge(3);
+    }
 }
-class Bajaj implements Vehicle{
-    public void brand(String BrandName){
-        System.out.println("Brand Name is: "+ BrandName);
-    }
-    public void makeModel(String Make_c){
-        System.out.println("Make and Model of the Vehicle: " + Make_c);
-    }
-    public void VehicleAge(int Years_c){
-        System.out.println("Age of vehicle: " + Years_c);
-
-    }
-
-}
-interface Vehicle{
-    public void brand(String BrandNae_c);
-    public void makeModel(String Make_c);
-    abstract void VehicleAge(int Years_c);
-}
-
-
-
