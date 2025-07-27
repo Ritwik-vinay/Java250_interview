d749334a05d44c6d99b624260c0ae284ae4fa8ba
ritwik vinay <ritwikvinay38@gmail.com>
2025-07-27 20:18:54 +0530

    HeadFirstJava-Ch1

---
+++ WeekTracker/JavaWeek2/SwapTwoNumbers.java
@@ -0,0 +1,21 @@
+package JavaWeek2;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        //Swap two Numbers with temp variable
+        //Enter two number use  a temp to Store the value
+        int num1 = 5;
+        int num2= 3;
+        int temp =0;
+        temp = temp+num1;
+        num1= num2;
+       num2 = temp;
+
+
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+
+    }
+}
