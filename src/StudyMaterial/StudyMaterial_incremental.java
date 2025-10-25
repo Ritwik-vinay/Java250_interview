1c3c3a686aaca7eaa675db43384f917adb4dc988
ritwik vinay <ritwikvinay38@gmail.com>
2025-10-25 21:56:48 +0530

    Study Material

---
+++ src/StudyMaterial/StudyMaterial_incremental.java
@@ -0,0 +1,25 @@
+package StudyMaterial;
+
+public class StudyMaterial_incremental {
+    public static void main(String[] args) {
+
+        //Increamental and Decremental Operator
+        //1. ++ 2. --
+        //PreIncremental or PreDecremental ++5, --5 1-5 or 1+5
+        // PostIncremental or Post Decremental
+
+        int age = 20;
+        int age2= age;
+        System.out.println(age);// 20
+        System.out.println(--age2); //19
+//        int a=12;
+//        System.out.println("*****************");
+//        for (int i=1; i<=10;i++){
+//
+//            System.out.println(a*i );
+//        }
+
+
+
+    }
+}
