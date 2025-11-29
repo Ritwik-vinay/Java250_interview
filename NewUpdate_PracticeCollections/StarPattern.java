dac1c18d292addc6bc4897519eae0f3d3e31c67f
ritwik vinay <ritwikvinay38@gmail.com>
2025-11-29 16:43:51 +0530

    Study Material

---
+++ NewUpdate_PracticeCollections/StarPattern.java
@@ -0,0 +1,11 @@
+public class StarPattern {
+    static void main() {
+        int num=5;
+        for (int i=0;i<=num;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
